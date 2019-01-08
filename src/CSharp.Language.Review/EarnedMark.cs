18472d0fdde9d3caf5429d98fb3308b3ad338e14
KeenanTrey <KeenanTrey@users.noreply.github.com>
2019-01-08 09:35:24 -0700

    no change

--- src/CSharp.Language.Review/EarnedMark.cs
+++ src/CSharp.Language.Review/EarnedMark.cs
@@ -4,29 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharp.Language.Review
-{
-    public class EarnedMark : WeightedMark
-    {
-        public int Possible { get; private set; }
-        private double _Earned;
-        public double Earned
-        {
-            get { return _Earned; }
-            set
-            {
-                if (value < 0 || value > Possible)
-                    throw new Exception("Invalid erned mark assigned");
-                _Earned = value;
-            }
-            public double Percent
-        { get { return (Earned / Possible) * 100;  } }
+//namespace CSharp.Language.Review
+//{
+//    public class EarnedMark : WeightedMark
+//    {
+//        //public int Possible { get; private set; }
+//        //private double _Earned;
+//        //public double Earned
+//        //{
+//        //    get { return _Earned; }
+//        //    set
+//        //    {
+//        //        if (value < 0 || value > Possible)
+//        //            throw new Exception("Invalid erned mark assigned");
+//        //        _Earned = value;
+//        //    }
+//        //    public double Percent
+//        //{ get { return (Earned / Possible) * 100;  } }
 
-        public double WeightedPercent
-        { get { return Percent * Weight / 100; } }
+//        //public double WeightedPercent
+//        //{ get { return Percent * Weight / 100; } }
 
-        public EarnedMark(WeightedMark markable Item, int possible)
-        }
+//        ////public EarnedMark(WeightedMark markable Item, int possible)
+//        //}
 
-    }
-}
+//    }
+//}
