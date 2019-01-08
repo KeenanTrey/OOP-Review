20483ea5840080a64f150cf0711518d753bc26df
KeenanTrey <KeenanTrey@users.noreply.github.com>
2019-01-08 09:34:20 -0700

    Earned mark started

--- src/CSharp.Language.Review/EarnedMark.cs
+++ src/CSharp.Language.Review/EarnedMark.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace CSharp.Language.Review
 {
-    class EarnedMark
+    public class EarnedMark : WeightedMark
     {
+        public int Possible { get; private set; }
+        private double _Earned;
+        public double Earned
+        {
+            get { return _Earned; }
+            set
+            {
+                if (value < 0 || value > Possible)
+                    throw new Exception("Invalid erned mark assigned");
+                _Earned = value;
+            }
+            public double Percent
+        { get { return (Earned / Possible) * 100;  } }
+
+        public double WeightedPercent
+        { get { return Percent * Weight / 100; } }
+
+        public EarnedMark(WeightedMark markable Item, int possible)
+        }
+
     }
 }
