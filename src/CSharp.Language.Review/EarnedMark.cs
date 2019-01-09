72dc4eb19b4b2af87a512f1a0c1ff2c929e65465
KeenanTrey <46493050+KeenanTrey@users.noreply.github.com>
2019-01-08 19:50:15 -0700

    Completed Review

--- src/CSharp.Language.Review/EarnedMark.cs
+++ src/CSharp.Language.Review/EarnedMark.cs
@@ -4,29 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//namespace CSharp.Language.Review
-//{
-//    public class EarnedMark : WeightedMark
-//    {
-//        //public int Possible { get; private set; }
-//        //private double _Earned;
-//        //public double Earned
-//        //{
-//        //    get { return _Earned; }
-//        //    set
-//        //    {
-//        //        if (value < 0 || value > Possible)
-//        //            throw new Exception("Invalid erned mark assigned");
-//        //        _Earned = value;
-//        //    }
-//        //    public double Percent
-//        //{ get { return (Earned / Possible) * 100;  } }
+namespace CSharp.Language.Review
+{
+    public class EarnedMark : WeightedMark
+    {
+        public int Possible { get; private set; }
+        private double _Earned;
+        public double Earned
+        {
+            get { return _Earned; }
+            set
+            {
+                if (value < 0 || value > Possible)
+                    throw new Exception("Invalid erned mark assigned");
+                _Earned = value;
+            }
+        }
+        public double Percent
+        { get { return (Earned / Possible) * 100; } }
 
-//        //public double WeightedPercent
-//        //{ get { return Percent * Weight / 100; } }
+        public double WeightedPercent
+        { get { return Percent * Weight / 100; } }
 
-//        ////public EarnedMark(WeightedMark markable Item, int possible)
-//        //}
+        public EarnedMark(WeightedMark markableItem, int possible, double earned) : this(markableItem.Name, markableItem.Weight, possible, earned)
+        {
 
-//    }
-//}
+        }
+
+        public EarnedMark(string name, int weight, int possible, double earned) : base(name, weight)
+        {
+            if (possible <= 0)
+                throw new Exception("Invalid possible marks");
+            Possible = possible;
+            Earned = earned;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})\t - {2}% ({3}/{4}) \t- Weighted Mark {5}%", Name, Weight, Percent, Earned, Possible, WeightedPercent);
+        }
+    }
+
+}
+
