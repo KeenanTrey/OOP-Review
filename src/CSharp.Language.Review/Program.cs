224dd86934f0e8e8eedf1aaf90ada98543d5d26c
KeenanTrey <46493050+KeenanTrey@users.noreply.github.com>
2019-01-11 11:25:11 -0700

    Added commments to program.cs

--- src/CSharp.Language.Review/Program.cs
+++ src/CSharp.Language.Review/Program.cs
@@ -1,41 +1,47 @@
-﻿using System;
+﻿//Identifies namespaces containing data types that are need to use or reference in in the code in this file
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Declares an area or named space in which we can place our programmer defined data types
+
 namespace CSharp.Language.Review
 {
+    //The namespace plus class name = fully qualified class name. The fully qualified class name  for program is CSharp.Language.Review.Program
     public class Program
     {
+        //An example of static field initialized to new random object
         private static Random rnd = new Random();
 
+        //Main method is entry point
         public static void Main(string[] args)
         {
+            //Body of the Main  method acts as the driver of my application
             Program app = new Program(args);
             
                 app.AssignMarks(30, 80);
 
                 foreach (Student person in app.Students)
                 {
-                    System.Console.WriteLine("Name: " + person.Name);
+                    Console.WriteLine("Name: " + person.Name);
                     foreach (EarnedMark item in person.Marks)
-                        System.Console.WriteLine("\t" + item);
-                }
-                      
-
-
-            
+                       Console.WriteLine("\t" + item);
+                }                                 
         }//eom
 
+        //This field is acting as a "backing store" for the Students property
         private List<Student> _students = new List<Student>();
 
+        //Property provides controlled access to the backing store (The field)
         public List<Student> Students
         {
             get { return _students; }
             set { _students = value; }
         }
 
+        //Construcor, used to ensure all fields/Properties have meaningful values
         public Program(string[] studentNames)
         {
             WeightedMark[] CourseMarks = new WeightedMark[4];
@@ -54,11 +60,16 @@ namespace CSharp.Language.Review
             }
         }
 
+        /// <summary>
+        /// This assigns a random mark to each student in the  <see cref="Students"/> property
+        /// </summary>
+        /// <param name="min">Minimum possible earned value for student's mark</param>
+        /// <param name="max">Maximum possible earned value for student's mark</param>
         public void AssignMarks(int min, int max)
         {
             foreach (Student person in Students)
                 foreach (EarnedMark item in person.Marks)
-                    item.Earned = (rnd.Next(min, max) / 100) * item.Possible;
+                    item.Earned = (rnd.Next(min, max) / 100.0) * item.Possible;
         }//eom
     }//eoc
 }//eon
